970614c8a9fc24b57cf5b0a356ab0ca96faf8de1
Vikas David Jonathan <vikas.jonathan@gmail.com>
2024-03-11 02:04:42 -0500

    Added Browser routes in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,34 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import AboutMe from './assets/pages/AboutMe.jsx'
+import Contact from './assets/pages/Contact.jsx'
+import Resume from './assets/pages/Resume.jsx'
+import Portfolio from './assets/pages/Portfolio.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router =createBrowserRoutes([
+  {
+    path: '/',
+    element: <App/>,
+    children:[
+      {
+        index: true,
+        element:<AboutMe/>,
+      },
+      {
+        path: '/Contact',
+        element: <Contact />,
+      },
+      {
+        path: '/Portfolio',
+        element: <Portfolio />,
+      },
+      {
+        path: '/Resume',
+        element: <Resume />,
+      },
+    ]
+  }
+]);
+ReactDOM.createRoot(document.getElementById('root')).render(<RouterProvider router={router} />);
+
